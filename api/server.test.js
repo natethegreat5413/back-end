c4612e6860f6770f6467f460de6280d3bcfcc3a7
Anthony Johnson <58479386+anthonyj713@users.noreply.github.com>
2020-05-28 17:08:20 -0600

    15 tests complete and run succesfully

--- api/server.test.js
+++ api/server.test.js
@@ -26,7 +26,7 @@ describe('server', () => {
             );
         });
 
-        test('returns an array of users', () => {
+        test('returns an empty array of users', () => {
             return (
                 supertest(server)
                 .get('/users')
@@ -67,6 +67,33 @@ describe('server', () => {
         })
     })
 
+    describe('POST /auth/login', () => {
+        let user3 = {
+            "username": "person3",
+            "password": "pw3!",
+        }
+        test('returns error code 500 when user doesnt exist in database', () => {
+           return (
+               supertest(server)
+               .post('/auth/login')
+               .send(user3)
+               .expect(500)
+           )
+        });
+        let user4 = {
+            "username":  '',
+            "password": "pw4!",
+        }
+        test('returns error code 400 when user field is empty', () => {
+           return (
+               supertest(server)
+               .post('/auth/login')
+               .send(user4)
+               .expect(400)
+           )
+        });
+    });
+
     describe("GET /plants", () => {
         test('should return http status code 200', () => {
             return (
@@ -78,11 +105,131 @@ describe('server', () => {
                 })
             );
         });
+        test('returns an empty array of plants', () => {
+            return (
+                supertest(server)
+                .get('/plants')
+                .then(res => {
+                    expect(res.body).toHaveLength(0);
+                })
+            )
+        })
     });
 
+    describe("POST /plants", () => {
+        test('having all fields creates a new plant', () => {
+            let plant1 = {
+                "user_id": 12,
+                "nickname": "Franklin",
+                "species": "Pirahna Plant",
+                "h2o_frequency": 5
+            }
+            return (
+                supertest(server)
+                .post('/plants')
+                .send(plant1)
+                .expect(201)
+            )
+        });
+        test('throws error when missing required field', () => {
+            let plant2 = {
+                "user_id": 12,
+                "species": "SADD Planta",
+                "h2o_frequency": 2
+            }
+            return (
+                supertest(server)
+                .post('/plants')
+                .send(plant2)
+                .expect(500)
+            )
+         });
+    });
+
+    describe("DELETE /plants/:id", () => {
+        test('delete plant that has a valid id',() => {
+            let plant3 = {
+                "id": 45,
+                "user_id": 3,
+                "nickname": "Trump",
+                "species": "Mango plant",
+                "h2o_frequency": 1,
+                "image_url": null,
+                "isWatered": null
+            }
+            return (
+                supertest(server)
+                .delete('/plants/45')
+                .then(res => {
+                    expect(res.type).toMatch(/json/i)
+                    expect(res.status).not.toBe(420)
+                })
+            )
+        })
+        test('fails to delete a plant that doesnt match id, returns 404 missing error ', () => {
+            let plant7 = {
+                "id": 7,
+                "user_id": 7,
+                "nickname": "Jesus",
+                "species": "Burning Bush",
+                "h2o_frequency": 7,
+                "image_url": null,
+                "isWatered": null
+            }
+            return (
+                supertest(server)
+                .delete('/plants/13')
+                .then(res => {
+                    expect(res.status).toBe(404)
+                    expect(res.status).not.toBe(420)
+                })
+            )
+        })
+     });
+    describe("PUT /plants/:id", () => {
+        test('delete plant that has a valid id',() => {
+            let plant5 = {
+                "id": 1,
+                "user_id": 3,
+                "nickname": "im out of puns",
+                "species": "sad face emoji",
+                "h2o_frequency": 1,
+                "image_url": null,
+                "isWatered": null
+            }
+            return (
+                supertest(server)
+                .put('/plants/1')
+                .then(res => {
+                    expect(res.type).toMatch(/json/i)
+                    expect(res.status).not.toBe(420)
+                })
+            )
+        })
+        test('fails to edit plant that doesnt match id, returns 404 missing error ', () => {
+            let plant2 = {
+                "id": 9,
+                "user_id": 4,
+                "nickname": "fin",
+                "species": "ished",
+                "h2o_frequency": 3,
+                "image_url": null,
+                "isWatered": null
+            }
+            return (
+                supertest(server)
+                .put('/plants/420')
+                .then(res => {
+                    expect(res.status).toBe(404)
+                    expect(res.status).not.toBe(420)
+                })
+            )
+        })
+    });
+
+});
 
 
 
 
 
-})
